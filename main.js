5aa1bac588e50e370f9485ec8702f3316cc04b5c
= <fundatory.milexinteam@gmail.com>
2026-05-12 16:00:14 +0200

    v. 1.0.0

---
+++ main.js
@@ -0,0 +1,35 @@
+const SERVER = "https://milexinteam.github.io/Denali-package-repo/repo.db.tar.gz";
+
+async function loadRepo() {
+  try {
+    const res = await fetch(SERVER);
+
+    if (!res.ok) {
+      throw new Error("Fetch failed");
+    }
+
+    const gz = new Uint8Array(await res.arrayBuffer());
+
+    // rozpakuj gzip → tar
+    const tarData = fflate.decompressSync(gz);
+
+    BrowserFS.configure({
+      fs: "TarFS",
+      options: {
+        data: tarData.buffer
+      }
+    }, () => {
+      const fs = BrowserFS.BFSRequire("fs");
+      console.log(fs.readdirSync("/"));
+    });
+
+  } catch (err) {
+    console.error("Błąd:", err);
+
+    // pokaż komunikat błędu
+    const errorEl = document.querySelector(".error");
+    if (errorEl) errorEl.classList.remove("hide");
+  }
+}
+
+loadRepo();
